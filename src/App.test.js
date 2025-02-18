e8b00cd2bec2ac66f338bbb268aa64eae1f1b598
tomedonovski <tomedonovski@gmail.com>
2025-02-18 21:28:37 +0100

    Downgrade react router version

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Task Manager header', () => {
+  render(
+    <App />
+  );
+
+  const headingElement = screen.getByText(/Task Manager/i);
+  expect(headingElement).toBeInTheDocument();
 });
